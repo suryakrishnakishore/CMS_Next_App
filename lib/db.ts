d293ae1c8648e8daf2644bf86438efb5e948ed34
suryakrishnakishore <suryakrishnakishore@gmail.com>
2025-09-01 23:41:10 +0530

    Database credentials changed

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Kis@20857', 
-  database: 'cms_db', 
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: parseInt(process.env.MYSQL_PORT as string),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
